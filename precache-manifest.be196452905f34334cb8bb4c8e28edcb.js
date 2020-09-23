f9a7d1d096dcbe0acae0a41b06daaa34888d0ae3
Siddhant <mamtajain81@gmail.com>
2020-09-23 19:13:03 +0530

    Updates

---
+++ precache-manifest.be196452905f34334cb8bb4c8e28edcb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45e2a07bfa815ea251b2b980026ad23c",
+    "url": "/Amazon-clone/index.html"
+  },
+  {
+    "revision": "fdc7e6efec05f0aeb875",
+    "url": "/Amazon-clone/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "265bb5d84d85f75445f8",
+    "url": "/Amazon-clone/static/js/2.768953d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Amazon-clone/static/js/2.768953d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdc7e6efec05f0aeb875",
+    "url": "/Amazon-clone/static/js/main.117a0cec.chunk.js"
+  },
+  {
+    "revision": "06670cc947c974a1e107",
+    "url": "/Amazon-clone/static/js/runtime-main.4c5c32ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Amazon-clone/static/media/logo.5d5d9eef.svg"
+  }
+]);
